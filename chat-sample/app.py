eb1c975613a37ac5c34f4bafa5021fdf0011e484
tokunagago <qazmlpplmzaq5@gmail.com>
2024-12-25 23:27:33 +0900

    add agent streamlit

--- chat-sample/app.py
+++ chat-sample/app.py
@@ -4,6 +4,8 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
 
 load_dotenv()
 
@@ -21,19 +23,26 @@ for message in st.session_state.messages:
 
 prompt = st.chat_input("What is up?")
 
+def create_agent_chain():
+    chat = ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+        streaming=True
+    )
+
+    tools = load_tools(["ddg-search", "wikipedia"])
+    return initialize_agent(tools, chat, agent=AgentType.OPENAI_FUNCTIONS)
+
 if prompt:
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.chat_message("assistant"):
-        chat = ChatOpenAI(
-            model_name=os.environ["OPENAI_API_MODEL"],
-            temperature=os.environ["OPENAI_API_TEMPERATURE"]
-        )
-        messages = [HumanMessage(content=prompt)]
-        response = chat(messages)
-        st.markdown(response.content)
+        callback = StreamlitCallbackHandler(st.container())
+        agent_chain = create_agent_chain()
+        response = agent_chain.run(prompt, callbacks=[callback])
+        st.markdown(response)
     
     # 応答を追加
     st.session_state.messages.append({"role": "assistant", "content": response})
